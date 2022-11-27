e76680ab3f5c9d76b7b74e4070eb665b181a618a
Ray <raysam.gas@gmail.com>
2022-11-28 01:38:04 +0700

    add use case

---
+++ src/Domains/threads/entities/_test/NewThread.test.js
@@ -0,0 +1,43 @@
+const NewThread = require('../NewThread');
+
+describe('a NewThread entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      title: 'asd',
+    };
+
+    // Action & Assert
+    expect(() => new NewThread(payload)).toThrowError(
+      'NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY'
+    );
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      title: true,
+      body: 'This is a kind of thread',
+    };
+
+    // Action & Assert
+    expect(() => new NewThread(payload)).toThrowError(
+      'NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
+  });
+
+  it('should create newThread object correctly', () => {
+    // Arrange
+    const payload = {
+      title: 'New Thread',
+      body: 'This is a kind of thread',
+    };
+
+    // Action
+    const { title, body } = new NewThread(payload);
+
+    // Assert
+    expect(title).toEqual(payload.title);
+    expect(body).toEqual(payload.body);
+  });
+});
